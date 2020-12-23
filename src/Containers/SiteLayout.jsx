ca450e5770f96800566e160de55deeb657892ba2
ritesh2606 <singh.rks224@gmail.com>
2020-12-23 22:55:35 +0530

    first commit

---
+++ src/Containers/SiteLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function mainLayout() {
+
+    return (
+        <div className='MainContainer'>
+            <div className='MainView'>
+
+            </div>
+            <div className='FormView'>
+
+            </div>
+        </div>
+    );
+}
